7ee8c41b3cbbc220f88d018d6d23ca431207d9e0
thejas-bk <thejasbk998@gmail.com>
2026-08-22 20:19:57 +0530

    progress

--- backend/main.go
+++ backend/main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+type User struct {
+	Name  string `json:"name"`
+	Email string `json:"email"`
+}
+
+func getUsersHandler(w http.ResponseWriter, r *http.Request) {
+	users := []User{
+		{Name: "One", Email: "one@gmail.com"},
+		{Name: "Two", Email: "two@gmail.com"},
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(users)
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	http.HandleFunc("/users", getUsersHandler)
+	fmt.Println("Server running at :8080")
+	http.ListenAndServe(":8080", nil)
 }
